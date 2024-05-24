aebdeaae9da603be7bc67f12bd3b5f277835a64a
Kristian <fich2402@gmail.com>
2024-05-25 00:33:48 +0200

    test: started queryhandler tests

---
+++ Webshop.Catalog.Solution/Webshop.Bookstore.Application.Test.Unit/QueryHandlerTests/Books/GetBooksQueryHandlerTests.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using FakeItEasy;
+using Webshop.BookStore.Application.Contracts.Persistence;
+using Webshop.BookStore.Application.Features.Book.Queries.GetBooks;
+
+namespace Webshop.Bookstore.Application.Test.Unit.CommandHandlerTests.Books;
+
+public class GetBooksQueryHandlerTests
+{
+    private readonly IBookRepository _fakeRepository;
+    private readonly IMapper _fakeMapper;
+    private readonly GetBooksQueryHandler _queryHandler;
+
+    public GetBooksQueryHandlerTests()
+    {
+        _fakeMapper = A.Fake<IMapper>();
+        _fakeRepository = A.Fake<IBookRepository>();
+        _queryHandler = new(_fakeRepository, _fakeMapper);
+    }
+
+    [Fact]
+    public async void GivenValidQuery_ShouldReturn_ListOfBookDtos()
+    {
+        
+    }
+}
