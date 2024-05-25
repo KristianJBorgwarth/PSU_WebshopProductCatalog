44941fdee369119b3c1a4120d64c1c9f73ae3a78
Kristian <fich2402@gmail.com>
2024-05-26 00:29:13 +0200

    forgot commit

--- Webshop.Catalog.Solution/Webshop.BookStore.Application/Features/Book/Commands/DeleteBook/DeleteBookCommand.cs
+++ Webshop.Catalog.Solution/Webshop.BookStore.Application/Features/Book/Commands/DeleteBook/DeleteBookCommand.cs
@@ -1,6 +1,9 @@
-﻿namespace Webshop.BookStore.Application.Features.Book.Commands.DeleteBook;
+﻿using MediatR;
+using Webshop.Domain.Common;
 
-public class DeleteBookCommand
+namespace Webshop.BookStore.Application.Features.Book.Commands.DeleteBook;
+
+public class DeleteBookCommand : IRequest<Result>
 {
-    
+    public int BookId { get; set; }
 }
--- Webshop.Catalog.Solution/Webshop.BookStore.Application/Features/Book/Commands/DeleteBook/DeleteBookCommandHandler.cs
+++ Webshop.Catalog.Solution/Webshop.BookStore.Application/Features/Book/Commands/DeleteBook/DeleteBookCommandHandler.cs
@@ -1,6 +1,20 @@
-﻿namespace Webshop.BookStore.Application.Features.Book.Commands.DeleteBook;
+﻿using MediatR;
+using Webshop.BookStore.Application.Contracts.Persistence;
+using Webshop.Domain.Common;
 
-public class DeleteBookCommandHandler
+namespace Webshop.BookStore.Application.Features.Book.Commands.DeleteBook;
+
+public class DeleteBookCommandHandler : IRequestHandler<DeleteBookCommand, Result>
 {
-    
+    private readonly IBookRepository _bookRepository;
+    public DeleteBookCommandHandler(IBookRepository bookRepository)
+    {
+        _bookRepository = bookRepository;
+    }
+
+
+    public async Task<Result> Handle(DeleteBookCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
