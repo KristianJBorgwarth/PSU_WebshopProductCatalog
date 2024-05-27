1db405b69b4f95dc782112e4040c116159504827
Kristian <fich2402@gmail.com>
2024-05-27 17:21:26 +0200

    feat: setup for create order endpoint

---
+++ Webshop.Catalog.Solution/Webshop.BookStore.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using Webshop.Application.Contracts.Persistence;
+using Webshop.BookStore.Domain.AggregateRoots;
+
+namespace Webshop.BookStore.Application.Contracts.Persistence;
+
+public interface IOrderRepository : IRepository<Order>
+{
+
+}
---
+++ Webshop.Catalog.Solution/Webshop.BookStore.Application/Features/Order/Requests/CreateOrderRequest.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Webshop.Domain.Common;
+
+namespace Webshop.BookStore.Application.Features.Order.Requests;
+
+public class CreateOrderRequest
+{
+    public int BuyerId { get; set; }
+
+    public class Validator : AbstractValidator<CreateOrderRequest>
+    {
+        public Validator()
+        {
+            RuleFor(x => x.BuyerId).NotNull().WithMessage(Errors.General.ValueIsRequired(nameof(BuyerId)).Code).GreaterThan(0)
+                .WithMessage(Errors.General.ValueTooSmall(nameof(BuyerId), 1).Code);
+        }
+    }
+}
