5d95a4abcedcf8317496c0b42c3232ecfe999cc8
Kristian <fich2402@gmail.com>
2024-05-28 12:35:36 +0200

    test: added endpoint test

--- Webshop.Catalog.Solution/Webshop.BookStore.Application/Features/Order/Requests/ApplyDiscountRequest.cs
+++ Webshop.Catalog.Solution/Webshop.BookStore.Application/Features/Order/Requests/ApplyDiscountRequest.cs
@@ -13,7 +13,7 @@ public class ApplyDiscountRequest
         public Validator()
         {
             RuleFor(x=> x.OrderId).NotNull().WithMessage(Errors.General.ValueIsRequired(nameof(OrderId)).Code).GreaterThanOrEqualTo(0)
-                .WithMessage(Errors.General.ValueTooSmall(nameof(OrderId), 1 ).Code);
+                .WithMessage(Errors.General.ValueTooSmall(nameof(OrderId), 0 ).Code);
             RuleFor(x => x.Discount).NotNull().WithMessage(Errors.General.ValueIsRequired(nameof(Discount)).Code);
         }
     }
